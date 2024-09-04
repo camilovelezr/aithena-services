36a91c9f9c45f6ad0f871eda0f19f5f66fe5183f
Camilo Velez <camilov61@gmail.com>
2024-09-03 23:40:23 -0400

    fix: revert chatmessage checks

--- src/aithena_services/llms/utils.py
+++ src/aithena_services/llms/utils.py
@@ -11,17 +11,12 @@ def cast_messages(messages: Sequence, is_dict: bool) -> Sequence[ChatMessage]:
     """Validate dict messages to Aithena Message and cast to li ChatMessage."""
     if is_dict:
         return [Message(**x).to_llamaindex() for x in messages]
-    if all(isinstance(x, ChatMessage) for x in messages):
-        aithena_messages = [
-            Message.from_llamaindex(x) for x in messages
-        ]  # for validation
-        return [x.to_llamaindex() for x in aithena_messages]
     return [x.to_llamaindex() for x in messages]  # x is Message
 
 
 def check_messages(messages: Sequence) -> bool:
     """Check messages."""
-    return all(isinstance(x, (Message, dict, ChatMessage)) for x in messages)
+    return all(isinstance(x, (Message, dict)) for x in messages)
 
 
 def check_and_cast_messages(messages: Sequence) -> Sequence[ChatMessage]:
