88c4b8f8e292e040e4cfbeade8da306c9f36655a
Camilo Velez <camilov61@gmail.com>
2024-09-25 00:42:59 -0400

    chore: update docker 0.0.1-dev1

--- docker/setup_ollama.py
+++ docker/setup_ollama.py
@@ -3,10 +3,10 @@
 This script will:
 * Pull llama3.1:latest to Ollama
 * Pull nomic-embed-text to Ollama
-* Update the config.json file for Aithena Services FastAPI
 """
 
 import logging
+import os
 
 import requests
 import typer
@@ -20,12 +20,12 @@ logger = logging.getLogger(__name__)
 
 app = typer.Typer(help="Setup Ollama for Aithena Services.")
 
+OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://localhost:11434")
+
 
 @app.command()
 def main(
-    ollama_host: str = "http://localhost:11434",
-    api_host: str = "http://localhost:8000",
-    internal_ollama_host: str = "http://192.0.0.89:11434",
+    ollama_host: str = OLLAMA_HOST,
 ):
     """Setup Ollama for Aithena Services."""
     logger.info("Pulling llama3.1:latest to Ollama")
@@ -49,29 +49,6 @@ def main(
         logger.error(f"Failed to pull nomic-embed-text: {e}")
     logger.info("Pulled nomic-embed-text to Ollama")
     logger.info("Updating config.json for Aithena Services - Chat")
-    try:
-        r = requests.put(
-            f"{api_host}/chat/list/update/ollama",
-            params={"overwrite": True, "url": internal_ollama_host},
-            timeout=20,
-        )
-        if r.json() != {"status": "success"}:
-            raise Exception(r.json()["detail"])
-    except Exception as e:
-        logger.error(f"Failed to update config.json for Aithena Services - Chat: {e}")
-    logger.info("Updated config.json for Aithena Services - Chat")
-    logger.info("Updating config.json for Aithena Services - Embed")
-    try:
-        requests.put(
-            f"{api_host}/embed/list/update/ollama",
-            params={"overwrite": True, "url": internal_ollama_host},
-            timeout=20,
-        )
-        if r.json() != {"status": "success"}:
-            raise Exception(r.json()["detail"])
-    except Exception as e:
-        logger.error(f"Failed to update config.json for Aithena Services - Embed: {e}")
-    logger.info("Updated config.json for Aithena Services - Embed")
     logger.info("Setup complete.")
 
 
